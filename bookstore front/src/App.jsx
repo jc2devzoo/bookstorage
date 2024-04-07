cce857fc69c6e0982966d59d88ec5f77f7fc0389
jcdevZoo <juliorocha207@gmail.com>
2024-04-06 23:09:08 -0300

    criação de rotas dentro do front

--- bookstore front/src/App.jsx
+++ bookstore front/src/App.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import {Routes, Route} from 'react-router-dom'
+import home from './pages/Home'
+import CreateBook from './page/CreateBook'
+import ShowBook from './pages/ShowBook'
+import EditBook from './pages/EditBook'
+import DeleteBook from './pages/DeleteBook'
 
 const App = () => {
   return (
-    <div>App</div>
+      <Routes>
+        < Route path = '/' element={ <home/>} />
+        < Route path = '/books/create' element={ <CreateBook/>} />
+        < Route path = '/books/details/:id' element = { <ShowBook/>} />
+        < Route path = '/books/edit/:ID' element = { <EditBook/>} />
+        < Route path = '/books/delete/:id' element = { <DeleteBook/>} />
+      </Routes>
   )
 }
 
